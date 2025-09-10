946d370e4db1ceaac398197fca83f2282d018ec8
Tamilvanan BN <tamilvanan.bn@gmail.com>
2025-09-10 21:25:36 +0530

    [3487] [Time Beats: 100.00%] [Memory Beats: 0.32%] - LeetPush

--- Maximum-Unique-Subarray-Sum-After-Deletion.rs
+++ Maximum-Unique-Subarray-Sum-After-Deletion.rs
@@ -1,8 +1,8 @@
-use std::collections::BTreeSet;
+use std::collections::HashSet;
 
 impl Solution {
     pub fn max_sum(nums: Vec<i32>) -> i32 {
-        let set: BTreeSet<_> = nums.into_iter().collect();
+        let set: HashSet<_> = nums.into_iter().collect();
         let (mut pos, mut neg) = (0, -1 << 31);
         for &x in &set {
             if x > 0 {
