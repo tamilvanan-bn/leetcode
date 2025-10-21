5f0817b8f7c9347a2d041bbf59393fa04c6ee772
Tamilvanan BN <tamilvanan.bn@gmail.com>
2025-10-21 22:30:41 +0530

    [3] [Time Beats: 98.26%] [Memory Beats: 47.09%] - LeetPush

---
+++ Longest-Substring-Without-Repeating-Characters.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  let l = 0,
+    r = 0,
+    n = s.length,
+    mx = 0,
+    hash = new Array(256).fill(-1);
+    while (r < n) {
+        if (hash[s[r].charCodeAt(0)] != -1) {
+            if (hash[s[r].charCodeAt(0)] >= l) {
+                l = hash[s[r].charCodeAt(0)] + 1;
+            }
+        }
+        const len = r - l + 1;
+        mx = Math.max(len, mx);
+        hash[s[r].charCodeAt(0)] = r++;
+    }
+    return mx;
+};
